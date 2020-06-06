6e3eea49c6978c6800d93480e935d9b3d8625414
mokasritus <66365291+mokasritus@users.noreply.github.com>
2020-06-06 08:29:28 +0200

    Variables, Types and Expressions in JavaScript-ready

---
+++ scripts.js
@@ -0,0 +1,59 @@
+console.log("mukodjel");
+
+let myString='This is a string';
+console.log("myString");
+
+let name = 'Rita';
+let greating = `Hello ${name}!`;
+console.log(`Hello ${name}!`);
+
+let charCount = 'Hello world'.length
+console.log('Hello+name'.length)
+
+let trueOrFalse = 5 === 5;
+console.log(trueOrFalse)
+trueOrFalse = 100 < 50;
+console.log(trueOrFalse)
+trueOrFalse = 'hello' !== 'world';
+console.log(trueOrFalse)
+trueOrFalse = 1 >= 2;
+console.log(trueOrFalse)
+trueOrFalse = 'blue'.length === 'green'.length;
+console.log(trueOrFalse)
+trueOrFalse = 3 < 4 && (5 > 6 || 7 < 8);
+console.log(trueOrFalse)
+
+let myArray = ['Claire', 'Mark', 'Susan'];
+let myOtherArray = [3, 12, true, 'Hello'];
+console.log(myArray[0]);
+myArray[0] = 'Kazmer';
+console.log(myArray[0]);
+console.log(myArray.length);
+myArray.push('Zita');
+console.log(myArray.length);
+
+
+let NameArray= ['Bela', 'Zita', 'Lea'];
+trueOrFalse = (`NameArray[2].length`)>5;
+console.log(trueOrFalse);
+NameArray.push('Szabolcs');
+let removedItem = NameArray.splice(0, 1)
+NameArray.splice(0,0,'Rita');
+console.log(NameArray);
+trueOrFalse = (NameArray.length)>4;
+console.log(trueOrFalse);
+
+var colors = new Array( "Orange", 'green', 'red');
+var hasManyColors=new Array("grey", "purple");
+
+let post ={
+    colors: ["Orange", 'green', 'red'],
+    hasManyColors: ['grey'],
+    };
+
+
+if (colors.length+hasManyColors.length >3) 
+{
+    console.log(trueOrFalse)
+} 
+    
